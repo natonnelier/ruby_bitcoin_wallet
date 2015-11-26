9164961824c2ec5e1331ffca9bc4cb6d6b18ce41
Nicolas Tonnelier <na.tonnelier@gmail.com>
2015-11-26 10:52:11 -0300

    Error message fixed

--- spec/ruby_bitcoin_wallet_spec.rb
+++ spec/ruby_bitcoin_wallet_spec.rb
@@ -30,33 +30,34 @@ describe RubyBitcoinWallet do
 
   describe 'Return blockchain data' do
     context 'with valid address' do
-      let(:test_address) { '1HawYer58J9Vy3iju1w7jsRVci5tzaxkwn' }
+      let(:valid_address) { '1HawYer58J9Vy3iju1w7jsRVci5tzaxkwn' }
 
-      it 'access address balance' do
-        balance = @wallet.address_balance(test_address)
+      it 'returns address balance' do
+        balance = @wallet.address_balance(valid_address)
         expect(balance[:final_balance]).to be_a_kind_of(Integer)
         expect(balance[:total_received]).to be_a_kind_of(Integer)
         expect(balance[:total_sent]).to be_a_kind_of(Integer)
       end
 
-      it 'access address transactions' do
-        transactions = @wallet.address_transactions(test_address)
+      it 'returns array of address transactions' do
+        transactions = @wallet.address_transactions(valid_address)
         expect(transactions).to be_a_kind_of(Array)
         expect(transactions.first['hash']).to be_a_kind_of(String)
+        expect(transactions.first['block_height']).to be_a_kind_of(Integer)
       end
     end
 
     context 'with invalid address' do
-      let(:test_address) { '2566358J9Vy3iju1w7jsRVci5tzaxkwn' }
+      let(:invalid_address) { '2566358J9Vy3iju1w7jsRVci5tzaxkwn' }
 
-      it 'access address balance' do
-        balance = @wallet.address_balance(test_address)
-        expect(balance).to eq("No data registered for address #{test_address}")
+      it 'returns invalid address message for balance' do
+        balance = @wallet.address_balance(invalid_address)
+        expect(balance).to eq("No data registered for address #{invalid_address}")
       end
 
-      it 'access address transactions' do
-        transactions = @wallet.address_transactions(test_address)
-        expect(transactions).to eq("No data registered for address #{test_address}")
+      it 'returns invalid address message for transactions' do
+        transactions = @wallet.address_transactions(invalid_address)
+        expect(transactions).to eq("No data registered for address #{invalid_address}")
       end
     end
   end
